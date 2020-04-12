b1b66cbbdc845e14130bea234cc3e9aa06fe8ded
Pranav <pranav2000joglekar@gmail.com>
2020-04-13 02:09:56 +0530

    Renamed getQuestions.py to Questions.py

---
+++ main.py
@@ -0,0 +1,25 @@
+#importing libraries
+import nltk
+import numpy as np
+import random
+import string
+import sys
+sys.path.append('src')
+import Questions
+
+#Strings used to identify and reply to casual first greetings
+GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up", "hey", "heyy","alright")
+GREETING_RESPONSES = ["hi", "hey", "hi there", "hello", "Feels nice talking to you"]
+questions = Questions.questions
+
+#Chatbot code. Runs until flag is True.
+flag=True
+print(random.choice(GREETING_INPUTS));
+while(flag==True):
+    user_response = input()
+    #user_response=user_response.lower()
+    if(user_response!='bye'):
+    	print(random.choice(questions))        
+    else:
+        flag=False
+        print("ROBO: Bye! take care..")
