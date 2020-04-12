d712c0148ba7a8c1e0c95056f1214f22de48ffac
Pranav <pranav2000joglekar@gmail.com>
2020-04-13 02:24:03 +0530

    Added function to choooseNextQuestion in Question.py and used this function in main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import Questions
 #Strings used to identify and reply to casual first greetings
 GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up", "hey", "heyy","alright")
 GREETING_RESPONSES = ["hi", "hey", "hi there", "hello", "Feels nice talking to you"]
-questions = Questions.questions
+questions = Questions.getQuestions()
 
 #Chatbot code. Runs until flag is True.
 flag=True
@@ -19,7 +19,8 @@ while(flag==True):
     user_response = input()
     #user_response=user_response.lower()
     if(user_response!='bye'):
-    	print(random.choice(questions))        
+    	#print(random.choice(questions))        
+        print(Questions.chooseNextQuestion(questions))
     else:
         flag=False
         print("ROBO: Bye! take care..")
--- src/Questions.py
+++ src/Questions.py
@@ -1,8 +1,15 @@
-file1 = open('resources/mainQuestions.txt', 'r') 
-questions=[]
-while True:
-	line=file1.readline();
-	questions.append(line);
-	if not line:
-		break
-file1.close()
+import random
+
+def getQuestions():
+    file1 = open('resources/mainQuestions.txt', 'r') 
+    questions=[]
+    while True:
+            line=file1.readline();
+            questions.append(line);
+            if not line:
+                    break
+    file1.close()
+    return questions
+
+def chooseNextQuestion(questions, constraints = None):
+    return random.choice(questions)
