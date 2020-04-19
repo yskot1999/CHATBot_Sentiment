1636349ed27ae882e13199e3f76a6d309c7e35dc
yash <kyash1999@gmail.com>
2020-04-19 12:52:23 +0530

    selection logic

--- src/Questions.py
+++ src/Questions.py
@@ -41,13 +41,15 @@ while True:
 file5.close()
 
 def chooseNextQuestion(emotion):
-    if(emotion==0):
+    if(emotion=="0"):
         return random.choice(A_questions)
-    elif(emotion==1):
+    elif(emotion=="1"):
         return random.choice(F_questions)
-    elif(emotion==2):
+    elif(emotion=="2"):
         return random.choice(H_questions)
-    elif(emotion==3):
+    elif(emotion=="3"):
         return random.choice(N_questions)
-    elif(emotion==4):
+    elif(emotion=="4"):
         return random.choice(S_questions)
+    else:
+        return random.choice(N_questions)
